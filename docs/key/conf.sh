07d855f340b5fac1c436f8f536af2c01c7f437db
Kaan S. Karadag <KaanSK@users.noreply.github.com>
2021-03-02 21:27:36 +0200

    Update conf.sh

--- docs/key/conf.sh
+++ docs/key/conf.sh
@@ -3,12 +3,12 @@
 sudo apt install -y wget gnupg2 gnupg-agent scdaemon pcscd git 
 mkdir -p ~/.gnupg
 wget https://raw.githubusercontent.com/KaanSK/kaansk.github.io/master/docs/key/gpg-agent.conf -P ~/.gnupg
-echo "export GPG_TTY=\"\$(tty)\"" >> ~/.bashrc
-echo "export SSH_AUTH_SOCK=\"/run/user/$UID/gnupg/S.gpg-agent.ssh\"" >> ~/.bashrc
-echo "gpg-connect-agent updatestartuptty /bye > /dev/null" >> ~/.bashrc
+echo "export GPG_TTY=\"\$(tty)\"" >> ~/.zshrc
+echo "export SSH_AUTH_SOCK=\"/run/user/$UID/gnupg/S.gpg-agent.ssh\"" >> ~/.zshrc
+echo "gpg-connect-agent updatestartuptty /bye > /dev/null" >> ~/.zshrc
 git config --global user.email "kaansk@gmail.com"
 git config --global user.name "KaanSK"
-gpg --keyserver pgp.circl.lu --recv-keys 0xC87A8F9F5B2F03CB
+gpg --keyserver http://pgp.circl.lu --recv-keys 0xC87A8F9F5B2F03CB
 ssh-add
 git config --global user.signingkey 0xC87A8F9F5B2F03CB
 gpg --card-status
